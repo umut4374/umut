97bd04f127c1d8185a9db98fffca749a71d4bc89
umut4374 <1umutaydin@gmail.com>
2025-03-08 19:16:07 +0100

    Delete child classima directory

--- child classima/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Ana temanın ve çocuk temanın stil dosyalarını içeri aktarma
-function classima_child_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-}
-add_action('wp_enqueue_scripts', 'classima_child_enqueue_styles');
